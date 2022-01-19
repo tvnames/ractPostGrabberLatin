2833bc8ce49c105b9bf9027fa4a1144c761c40b3
Andrew Parsons <parsonsaaa@gmail.com>
2022-01-18 16:49:01 -0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Posts from "./components/Posts";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="/logo.svg" className="App-logo" alt="logo" />
+        <h1>React Posts Sharer</h1>
+      </header>
+      <Posts />
+    </div>
+  );
+}
+
+export default App;
